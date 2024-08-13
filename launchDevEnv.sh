38e55326f62171f2a4021c631d28014c89e47e73
SteveMieskoski <steveMieskoski@gmail.com>
2024-08-13 13:28:06 -0400

    remove unneeded files, add usage to launch bash script

--- launchDevEnv.sh
+++ launchDevEnv.sh
@@ -11,17 +11,19 @@ if ! pgrep docker 2>/dev/null; then
   exit
 fi
 
-#cd ./docker || exit
-#
-#docker compose down
-#
-#docker compose up -d
+
+help(){
+  echo "Script to launch docker for local geth blockchain and indexer infrastructure components "
+  echo "Usage: launchDevEnv.sh [-a] [-i] [-b]"
+  echo " -a - launch both local blockchain and indexer infrastructure in docker"
+  echo " -b - only launch local blockchain in docker"
+  echo " -i - only launch local indexer infrastructure in docker"
+  echo " -h - show this help"
+}
+
 BLOCKCHAIN=0
 INFRA=0
-FUZZ=0
-FUZZTWO=0
-FUZZBLOB=0
-FUZZOTHER=0
+HELP=0
 
 POSITIONAL=()
 while [[ $# -gt 0 ]]
@@ -29,33 +31,26 @@ do
 key="$1"
 
 case $key in
+    -a)
+    BLOCKCHAIN=1
+    INFRA=1
+    shift
+    ;;
     -b)
     BLOCKCHAIN=1
-    shift # past argument
+    shift
     ;;
     -i)
     INFRA=1
-    shift # past argument
+    shift
     ;;
-    -f)
-    FUZZ=1
-    shift # past argument
+    -h)
+    HELP=1
+    shift
     ;;
-    -f2)
-    FUZZTWO=1
-    shift # past argument
-    ;;
-    -fb)
-    FUZZBLOB=1
-    shift # past argument
-    ;;
-    -fo)
-    FUZZOTHER=1
-    shift # past argument
-    ;;
-    *)    # unknown option
-    POSITIONAL+=("$1") # save it in an array for later
-    shift # past argument
+    *)
+    POSITIONAL+=("$1")
+    shift
     ;;
 esac
 done
@@ -64,15 +59,14 @@ set -- "${POSITIONAL[@]}"
 
 if [ $BLOCKCHAIN -eq 0 ]; then
   if [ $INFRA -eq 0 ]; then
-    if [ $FUZZ -eq 0 ]; then
-      if [ $FUZZBLOB -eq 0 ]; then
-        if [ $FUZZOTHER -eq 0 ]; then
-          BLOCKCHAIN=1
-          INFRA=1
-        fi
-      fi
-    fi
+    help
+    exit
   fi
+fi
+
+if [[ HELP -ne 0 ]]; then
+    help
+    exit
 fi
 
 cd ./docker || exit
@@ -100,50 +94,4 @@ if [[ $INFRA -ne 0 ]]; then
 
   cd .. || exit
 fi
-
-if [[ $FUZZ -ne 0 ]]; then
-
-  cd ./fuzz || exit
-
-  ./livefuzzer spam --sk 0x62014863ea920a2fe27631a7f6b2732e2a554787e3f564a2f9113b351dc7c4ec --slot-time 20 --gaslimit 50000 --accounts 20 --txcount 2
-
-  cd .. || exit
-fi
-
-if [[ $FUZZTWO -ne 0 ]]; then
-
-  cd ./fuzz || exit
-
-  ./livefuzzer spam --sk 0x4f9c794dba16f0a3e252b8e202c824c17ad6252b8424d1d17094717184461e8b --slot-time 20 --gaslimit 50000 --accounts 20 --txcount 2
-
-  cd .. || exit
-fi
-
-if [[ $FUZZBLOB -ne 0 ]]; then
-
-  cd ./spammer || exit
-
-   ./blob-spammer combined -p 62014863ea920a2fe27631a7f6b2732e2a554787e3f564a2f9113b351dc7c4ec -h http://localhost:8545 -b 2 -t 3 --max-pending 3
-
-  cd .. || exit
-fi
-
-if [[ $FUZZOTHER -ne 0 ]]; then
-
-  cd ./fuzz || exit
-
-  ./livefuzzer blobs --accounts 20 --txcount 2
-
-  cd .. || exit
-fi
-
-
-
-#cd ..
-#
-#cd ./docker/infrastructure || exit
-#
-#docker compose down
-#
-#docker compose up -d
 
